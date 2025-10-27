0b1501b4d458cf169b503cb228bbb5c2c893c16b
Ey Pacha! <pachaguionbajo@gmail.com>
2025-10-27 20:32:44 -0300

    implement demo function and add requirements file

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+
+
+
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+def demo(screen):
+    from asciimatics.effects import Print
+    from asciimatics.renderers import Rainbow
+    effects = [
+        Cycle(
+            screen,
+            FigletText("GHOSTKEY", font='big'),
+            int(screen.height / 2 - 8)
+        ),
+        Print(
+            screen,
+            Rainbow(screen, FigletText("ENTER TO START", font="small")),
+            int(screen.height / 2 + 8)
+        ),
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 500)])
+
+
 def main():
-    print(r"""
- ____    __                       __          __                          
-/\  _`\ /\ \                     /\ \__      /\ \                         
-\ \ \L\_\ \ \___     ___     ____\ \ ,_\     \ \ \/'\      __   __  __    
- \ \ \L_L\ \  _ `\  / __`\  /',__\\ \ \/      \ \ , <    /'__`\/\ \/\ \   
-  \ \ \/, \ \ \ \ \/\ \L\ \/\__, `\\ \ \_      \ \ \\`\ /\  __/\ \ \_\ \  
-   \ \____/\ \_\ \_\ \____/\/\____/ \ \__\      \ \_\ \_\ \____\\/`____ \ 
-    \/___/  \/_/\/_/\/___/  \/___/   \/__/       \/_/\/_/\/____/ `/___/> \ 
-                                                                    /\___/
-                                                                    \/__/  
-    """)
+    Screen.wrapper(demo)
     # Aquí irá la lógica del juego
 
 if __name__ == "__main__":
