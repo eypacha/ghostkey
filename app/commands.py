18e667ddd28a10aa0b86188f725304aeac81fcab
Ey Pacha! <pachaguionbajo@gmail.com>
2025-10-27 21:05:58 -0300

    feat: add initial terminal commands for game level 1

---
+++ app/commands.py
@@ -0,0 +1,14 @@
+# commands.py - Lista de comandos de terminal para el juego
+
+COMMANDS_LEVEL_1 = [
+    "ls -la /home/user",
+    "cd /var/www",
+    "pwd",
+    "mkdir -p /tmp/session",
+    "rm -f /tmp/file.tmp",
+    "touch \"/tmp/new note.txt\"",
+    "cp file.txt /backup/file.txt",
+    "mv image.png ~/Pictures/",
+    "chmod 600 ~/.ssh/id_rsa",
+    "ssh user@192.168.0.50 -p 2222"
+]
